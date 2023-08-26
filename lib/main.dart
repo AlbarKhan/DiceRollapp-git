e9ef8c6bdc59691e2683fd625214fbc9a361fad3
unknown <albarkhan60@gmail.com>
2023-08-26 13:07:33 +0530

    Added AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:dart_flutter_basic_1/gradient_background.dart';
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: GradientBackground(Color.fromARGB(255, 243, 224, 55),Color.fromARGB(255, 247, 228, 121))
-      )
+        appBar: AppBar(
+          title: const Text(
+            'Dice Roller App',
+          ),
+        ),
+        body: const GradientBackground(
+          Color.fromARGB(255, 243, 224, 55),
+          Color.fromARGB(255, 247, 228, 121),
+        ),
+      ),
     ),
   );
 }
-
